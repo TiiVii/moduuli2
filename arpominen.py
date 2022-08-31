7a5c4e55482c6c5b5657205ea0081b50da9cc09d
tytti <tytti.m.j.vilen@gmail.com>
2022-08-31 11:12:49 +0300

    moduuli2

--- arpominen.py
+++ arpominen.py
@@ -4,9 +4,9 @@ ensimmainen3 = randint(0, 9)
 toinen3 = randint(0, 9)
 kolmas3 = randint(0, 9)
 
-ensimmainen4 = randint(0, 9)
-toinen4 = randint(0, 9)
-kolmas4 = randint(0, 9)
+ensimmainen4 = randint(1, 6)
+toinen4 = randint(1, 6)
+kolmas4 = randint(1, 6)
 neljas = randint(1, 6)
 
 print('Kolminumeroinen koodi:')
